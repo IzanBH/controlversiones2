916fee658114e6777248cd49f7a61c1999703333
John Doe <johndoe@example.com>
2026-05-25 14:52:17 +0200

    izan

---
+++ bin/controlversiones2.dart
@@ -0,0 +1,5 @@
+import 'package:controlversiones2/controlversiones2.dart' as controlversiones2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${controlversiones2.calculate()}!');
+}
---
+++ test/controlversiones2_test.dart
@@ -0,0 +1,8 @@
+import 'package:controlversiones2/controlversiones2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
